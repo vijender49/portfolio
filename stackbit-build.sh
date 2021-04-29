af10ae1ac004d10d005b72babd7d9acd116917e0
bijender <ak266092@gmail.com>
2021-04-29 15:42:43 +0530

    as new commit

---
+++ stackbit-build.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -e
+set -o pipefail
+set -v
+
+echo "stackbit-build.sh: start build"
+
+# fetch data from CMS through stackbit-pull
+npx @stackbit/stackbit-pull --stackbit-pull-api-url=https://api.stackbit.com/pull/608a6019ef308400162232ce
+
+# build site
+npm run build
+
+echo "stackbit-build.sh: finished build"
